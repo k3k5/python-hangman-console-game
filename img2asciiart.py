d4ea21c91267f146b5cdfbb319e5db42b45facef
k3k5 <sebastiankeck7@gmail.com>
2017-01-11 22:22:36 +0100

    Update img2asciiart.py

--- img2asciiart.py
+++ img2asciiart.py
@@ -4,21 +4,20 @@ Creates an ascii art image from an arbitrary image
 Created on 7 Sep 2009
  
 @author: Steven Kay
-
-@website: https://stevendkay.wordpress.com/2009/09/08/generating-ascii-art-from-photographs-in-python/
-
-
 '''
  
 from PIL import Image
 import random
 from bisect import bisect
- 
-# greyscale.. the following strings represent
-# 7 tonal ranges, from lighter to darker.
-# for a given pixel tonal level, choose a character
-# at random from that range.
- 
+
+
+'''
+Original source code (deleted comments; https://stevendkay.wordpress.com/2009/09/08/generating-ascii-art-from-photographs-in-python/):
+
+from PIL import Image
+import random
+from bisect import bisect
+
 greyscale = [
             " ",
             " ",
@@ -29,26 +28,15 @@ greyscale = [
             "W8KMA",
             "#%$"
             ]
- 
-# using the bisect class to put luminosity values
-# in various ranges.
-# these are the luminosity cut-off points for each
-# of the 7 tonal levels. At the moment, these are 7 bands
-# of even width, but they could be changed to boost
-# contrast or change gamma, for example.
- 
+
+
 zonebounds=[36,72,108,144,180,216,252]
- 
-# open image and resize
-# experiment with aspect ratios according to font
- 
-im=Image.open(r"hangman.jpg")
-im=im.resize((80, 35),Image.BILINEAR)
+
+
+im=Image.open(r"c:\test.jpg")
+im=im.resize((160, 75),Image.BILINEAR)
 im=im.convert("L") # convert to mono
- 
-# now, work our way over the pixels
-# build up str
- 
+
 str=""
 for y in range(0,im.size[1]):
     for x in range(0,im.size[0]):
@@ -57,5 +45,54 @@ for y in range(0,im.size[1]):
         possibles=greyscale[row]
         str=str+possibles[random.randint(0,len(possibles)-1)]
     str=str+"\n"
- 
+
 print str
+
+'''
+
+# use:  import img2asciiart
+#       print img2asciiart.img2asciiart().create_ascii_art("game_over.jpg")
+
+class img2asciiart:
+
+    def create_ascii_art(self, img_src):
+        greyscale = [
+                    " ",
+                    " ",
+                    ".,-",
+                    "_ivc=!/|\\~",
+                    "gjez2]/(YL)t[+T7Vf",
+                    "mdK4ZGbNDXY5P*Q",
+                    "W8KMA",
+                    "#%$"
+                    ]
+
+        # using the bisect class to put luminosity values
+        # in various ranges.
+        # these are the luminosity cut-off points for each
+        # of the 7 tonal levels. At the moment, these are 7 bands
+        # of even width, but they could be changed to boost
+        # contrast or change gamma, for example.
+
+        zonebounds=[36,72,108,144,180,216,252]
+
+        # open image and resize
+        # experiment with aspect ratios according to font
+
+        im=Image.open(img_src)
+        im=im.resize((80, 35),Image.BILINEAR)
+        im=im.convert("L") # convert to mono
+
+        # now, work our way over the pixels
+        # build up str
+
+        str=""
+        for y in range(0,im.size[1]):
+            for x in range(0,im.size[0]):
+                lum=255-im.getpixel((x,y))
+                row=bisect(zonebounds,lum)
+                possibles=greyscale[row]
+                str=str+possibles[random.randint(0,len(possibles)-1)]
+            str=str+"\n"
+
+        print str
